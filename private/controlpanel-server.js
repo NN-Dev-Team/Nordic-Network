ce9cf4ba183b75a9b8c25a6a73e77a1843b3bd3d
Tropic <tropical.productions.scratch@gmail.com>
2016-01-09 23:08:00 +0100

    Update and rename control-panel.js to controlpanel-server.js
    WIP

---
+++ private/controlpanel-server.js
@@ -0,0 +1,31 @@
+// THIS FILE IS NOT NEAR DONE
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+
+var values = [];
+
+fs.readFile('../public/properities.txt', 'utf8', function (err, data) {
+	if (err) {
+		return console.log(err);
+	}
+	values = data.split("\n");
+});
+
+var port = values[1];
+
+app.get('/', function(req, res){ // For those who think res means resolution it doesn't; it means result
+	res.sendFile(__dirname + '../public/controlpanel.html'); // ethernity4ever what is the name of the control panel html file?
+});
+
+io.on('connection', function(socket){
+	socket.on('example', function(data){
+		io.emit('example received', data);
+	});
+});
+
+http.listen(port, function(){ # Port 15015
+	console.log('listening on *:' + port);
+});
