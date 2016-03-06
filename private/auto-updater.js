27ce9a7531f5209653ceed3a52c090f6c5bac863
TropicSapling <tropical.productions.scratch@gmail.com>
2016-03-06 19:09:56 +0100

    [WIP] Added auto updater
    - Added auto-updater.js which updates to newer mc versions, currently
    http-sync isn't working on windows though and therefore I can't test if
    it works or not atm.

---
+++ private/auto-updater.js
@@ -0,0 +1,79 @@
+var httpsSync = require('http-sync');
+
+Date.prototype.getWeek = function() {
+	var date = new Date(this.getTime());
+	date.setHours(0, 0, 0, 0);
+	date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+	var week1 = new Date(date.getFullYear(), 0, 4);
+	return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
+}
+
+function getMCSnapshotUrl(year, week) {
+	var base_url = "https://s3.amazonaws.com/Minecraft.Download/versions/";
+	var url = base_url;
+	var sUrl = 0;
+	var now = new Date();
+	var thisYear = now.getFullYear();
+	var thisWeek = now.getWeek();
+	var strWeek = thisWeek.toString();
+	var strYear = year.toString();
+	if(strWeek.substring(0, 1) != "0") {
+		strWeek = "0" + week;
+	}
+	
+	do {
+		var snapshot = strYear.substring(2) + "w" + strWeek;
+		var snapList = ['a', 'b', 'c', 'd', 'e', 'f'];
+		
+		for(i = 0; i < snapList.length; i++) {
+			url = base_url + snapshot + "/minecraft_server." + snapshot + snapList[i] + ".jar";
+			
+			var opts = {
+				method: 'HEAD',
+				host: url,
+				protocol: 'https'
+			};
+			
+			var req = httpsSync.request(opts);
+			var res = req.end();
+			if(res.statusCode != 403) {
+				sUrl = url;
+			}
+		}
+	} while(year != thisYear || week != thisWeek);
+	
+	return sUrl;
+}
+
+function getMCPreUrl(version) {
+	var thisVersion = version;
+	var base_url = "https://s3.amazonaws.com/Minecraft.Download/versions/";
+	var url = base_url;
+	var preUrl = 0;
+	
+	while(version + 0.2 > thisVersion) {
+		for(i = 1; i < 10; i++) {
+			url = base_url + thisVersion + "-pre" + i + "/minecraft_server." + thisVersion + "-pre" + i + ".jar";
+			
+			var opts = {
+				method: 'HEAD',
+				host: url,
+				protocol: 'https'
+			};
+			
+			var req = httpsSync.request(opts);
+			var res = req.end();
+			if(res.statusCode != 403) {
+				preUrl = url;
+			}
+		}
+		
+		thisVersion += 0.1;
+	}
+	
+	return preUrl;
+}
+
+// Testing below:
+console.log(getMCSnapshotUrl(2016, 9));
+console.log(getMCPreUrl(1.9));
