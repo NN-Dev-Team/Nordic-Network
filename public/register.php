ba7df33fd24efda0e81f40fc9d90519a9ff82520
TropicSapling <tropical.productions.scratch@gmail.com>
2016-03-18 20:31:40 +0100

    Created basic registration page
    - Added register.php
    - Moved ad code from index-proto.css to other-proto.css

---
+++ public/register.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title>Nordic Network</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<link rel="stylesheet" href="/css/nav-proto.css">
+		<link rel="stylesheet" href="/css/other-proto.css">
+		<link rel="stylesheet" href="/css/register.css">
+		<link rel="stylesheet" href="/css/footer-proto.css">
+	</head>
+	<body>
+		<?php include 'navbar-proto.html'; ?>
+		<div id="ad-box-outer">
+			<div class="ad" id="ad-box-inner"></div>
+		</div>
+		<div class="ad" id="ad-box-mobile"></div>
+		<?php include 'footer-proto.html'; ?>
+	</body>
+</html>
