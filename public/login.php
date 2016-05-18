6b7a5963136d271a7c8d91546f19a9fbd22bf6d0
Tropic <tropical.productions.scratch@gmail.com>
2016-05-18 17:12:25 +0200

    Revert "Add control panel"

--- public/login.php
+++ public/login.php
@@ -5,7 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		
 		<!-- Load custom CSS from file -->    
-		<link rel="stylesheet" href="css/styles.css">
+		<link rel="stylesheet" href="/css/styles.css">
 		<!-- Load bootstrap stylesheet from CDN -->
 		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 		<!-- Load JQuery from CDN -->
@@ -16,7 +16,7 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.0/jquery.waypoints.js"></script>
     </head>
 	
-    <body id="logregbody">
+    <body>
 		<nav id="nav-nomargin" class="navbar navbar-default navbar-fixed-top">
 			<div class="container-fluid">
 				<div class="navbar-header">
