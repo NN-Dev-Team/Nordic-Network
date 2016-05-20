c351b582ef53938e4c17e99de004635338e764d5
Tropic <tropical.productions.scratch@gmail.com>
2016-05-20 20:36:21 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -14,6 +14,8 @@
 		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		<!-- Load JQuery Waypoints from CDN -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.0/jquery.waypoints.js"></script>
+		<!-- Load Fontastic CSS from file -->
++        	<link rel="stylesheet" href="fontastic.css">
 	</head>
 	<body id="top">
 		<!-- Nav -->
