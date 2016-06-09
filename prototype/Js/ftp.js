a40302322d86177eae02a18010351387af9e0986
Eddie teddy Smale <tedward2014@gmail.com>
2016-06-09 16:04:39 +0100

    Update ftp.js

--- prototype/Js/ftp.js
+++ prototype/Js/ftp.js
@@ -1,1 +1,10 @@
-
+document.getElementById('UploadFile').addEventListener('change', readFile, false);
+function readFile (evt) {
+  var files = evt.target.files;
+  var file = files[0];           
+  var reader = new FileReader();
+  reader.onload = function() {
+    console.log(this.result);            
+  }
+  reader.readAsText(file)
+}
