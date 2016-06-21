e868de9a50cacacf71e5669fb18fb2743533f526
TropicSapling <tropical.productions.scratch@gmail.com>
2016-06-21 21:48:33 +0200

    Console themes are now (almost) fully functional! :D

--- public/js/controlpanel-console.js
+++ public/js/controlpanel-console.js
@@ -29,7 +29,14 @@ $(document).ready(function() {
 		$('#console textarea').text(data);
 	});
 	
-	$("#consoleinput").on("keydown",function sendCMD(e) {
+	$("#consoleinput").on("keydown", function sendCMD(e) {
+		if(e.keyCode == 13) {
+			socket.emit('console-cmd', $(this).val());
+			$(this).val("");
+		}
+	});
+	
+	$("#console textarea").on("keydown", function sendCMD(e) {
 		if(e.keyCode == 13) {
 			socket.emit('console-cmd', $(this).val());
 			$(this).val("");
@@ -38,10 +45,20 @@ $(document).ready(function() {
 });
 
 function changeTheme(state) {
+	var device_width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+	
 	if(state == 0) {
 		// Console
 		$('#console').css('border-radius', 0);
 		$('#console').css('background-color', "black");
+		if(device_width < 768) {
+			$('#console').css('height', "calc(100vh - 151px)");
+		} else if(device_width > 1919) {
+			$('#console').css('height', "calc(65vh - 40px)");
+		} else {
+			$('#console').css('height', "calc(60vh - 30px)");
+		}
+		$('#console').css('border', "1px solid gray");
 		
 		// Console buttons
 		$('#cnslchangecolor').css('background-color', "black");
@@ -50,25 +67,19 @@ function changeTheme(state) {
 		$('#cnslautochat').css('color', "white");
 		
 		// Console IO
-		$('#console').prepend($('#consoleinput'));
-		$('#consoleinput').css('width', $('#console textarea').css('width'));
-		$('#consoleinput').css('height', $('#console textarea').css('height'));
-		$('#console textarea').remove();
-		$('#consoleinput').css('color', "");
-		$('#consoleinput').css('font-family', "");
-		$('#consoleinput').css('font', "");
-		$('#consoleinput').css('color', "");
-		$('#consoleinput').css('border-bottom-left-radius', "");
-		$('#consoleinput').css('border-bottom-right-radius', "");
-		$('#consoleinput').css('border-bottom', "");
-		$('#consoleinput').css('border-top', "");
-		$('#consoleinput').css('border-left', "");
-		$('#consoleinput').css('background-color', "black");
-		$('#consoleinput').css('border', "none");
-		$('#consoleinput').css('resize', "none");
-		$('#consoleinput').css('border-top-left-radius', "10px");
-		$('#consoleinput').css('float', "left");
-		$('#consoleinput').css('border-right', "1px solid gray");
+		$('#consoleinput').css('display', 'none');
+		$('#console textarea').removeAttr('readonly');
+		$('#console textarea').css('border-top-left-radius', 0);
+		$('#console textarea').css('background-color', "black");
+		$('#console textarea').css('border-right', "1px solid gray");
+		$('#console textarea').css('color', "white");
+		if(device_width < 768) {
+			$('#console textarea').css('height', "calc(100vh - 153vh)");
+		} else if(device_width > 1919) {
+			$('#console textarea').css('height', "calc(65vh - 42px)");
+		} else {
+			$('#console textarea').css('height', "calc(60vh - 32px)");
+		}
 		
 		// Console theme changer button
 		$('#cnslchangecolor').attr('onclick', "changeTheme(1)");
@@ -79,6 +90,14 @@ function changeTheme(state) {
 		// Console
 		$('#console').css('border-radius', "10px");
 		$('#console').css('background-color', "white");
+		if(device_width < 768) {
+			$('#console').css('height', "calc(100vh - 126px)");
+		} else if(device_width > 1919) {
+			$('#console').css('height', "65vh");
+		} else {
+			$('#console').css('height', "60vh");
+		}
+		$('#console').css('border', "1px solid black");
 		
 		// Console buttons
 		$('#cnslchangecolor').css('background-color', "white");
@@ -87,8 +106,19 @@ function changeTheme(state) {
 		$('#cnslautochat').css('color', "");
 		
 		// Console IO
-		$('#consoleinput').css('color', "");
-		$('#consoleinput').css('border-top', "1px solid black");
+		$('#console textarea').attr('readonly', "");
+		$('#console textarea').css('border-top-left-radius', "10px");
+		$('#console textarea').css('background-color', "");
+		$('#console textarea').css('border-right', "1px solid black");
+		$('#console textarea').css('color', "");
+		if(device_width < 768) {
+			$('#console textarea').css('height', "calc(100vh - 151px)");
+		} else if(device_width > 1919) {
+			$('#console textarea').css('height', "calc(65vh - 40px)");
+		} else {
+			$('#console textarea').css('height', "calc(60vh - 30px)");
+		}
+		$('#consoleinput').css('display', 'inline');
 		
 		// Console theme changer button
 		$('#cnslchangecolor').attr('onclick', "changeTheme(0)");
