f24631b876e8c5be7c84ceb2eed9661a9b480388
TropicSapling <tropical.productions.scratch@gmail.com>
2016-12-03 17:20:36 +0100

    Updated bootstrap

--- public/index.php
+++ public/index.php
@@ -12,12 +12,12 @@
 		<link href="https://fonts.googleapis.com/css?family=David+Libre|Yatra+One" rel="stylesheet"> 
         <!-- Load Odometer Theme CSS from file -->
         <link rel="stylesheet" href="/css/odometer-theme-minimal.css" />
-		<!-- Load bootstrap CSS from CDN -->
-		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 		<!-- Load JQuery from CDN -->
-		<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+		<script src="http://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+		<!-- Load bootstrap CSS from CDN -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<!-- Load bootstrap JS from CDN -->
-		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 		<!-- Load Socket.io -->
 		<script src="https://cdn.socket.io/socket.io-1.4.5.js"></script>
 		<!-- Load JQuery Waypoints from CDN -->
