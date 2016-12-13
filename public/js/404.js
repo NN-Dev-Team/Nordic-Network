76c733296a56463f3dd56eaafd599033a4cd0919
TropicSapling <tropical.productions.scratch@gmail.com>
2016-12-13 16:48:06 +0100

    Server pages client code should now be working

--- public/js/404.js
+++ public/js/404.js
@@ -17,7 +17,36 @@ $(document).ready(function(){
 			console.log("ERROR: Unable to connect to server.");
 			show404();
 		} else {
-			socket.emit('get-user-page', {"url": window.location.href});
+			var serv_type;
+			var usr_id = "";
+			var url = window.location.href;
+			var c = 0;
+			
+			while(url[c] != "/") {
+				c++;
+			}
+			c++;
+			
+			if(url.substring(c, c + 2) == "u/" && !isNaN(url[c + 2])) {
+				c += 2;
+				while(url[c] != "/" && !isNaN(url[c])) {
+					usr_id += url[c];
+					c++;
+				}
+				
+				if(url[c] != "/") {
+					show404();
+				} else {
+					c++;
+					if(url.substring(c, c + 7) == "server") {
+						socket.emit('get-user-page', {"type": 0, "id": Number(usr_id)});
+					} else { // Type 1 has not been decided what it is yet so no code for it yet
+						show404();
+					}
+				}
+			} else {
+				show404();
+			}
 			
 			socket.on('show-404', show404);
 		}
