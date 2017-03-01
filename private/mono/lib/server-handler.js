18b683ee68bacd2523fdba4fd1d6118cab70b44e
TropicSapling <tropical.productions.scratch@gmail.com>
2017-03-01 20:31:42 +0100

    Bugfixes

--- private/mono/lib/server-handler.js
+++ private/mono/lib/server-handler.js
@@ -1,19 +1,21 @@
 var mkdir = require('mkdirp');
 var user = require('./user-lib.js');
 
+////////////////////////////////    SERVER CREATION    ////////////////////////////////
+
 exports.create = function createServer(data, IP, callback) {
 	if(typeof data.session != 'string' || (data.session).length < 24) {
 		return console.log("[!] Possible hacker detected (with IP: " + IP + ")");
 	} else if(Math.round((new Date).getTime() / 60000 > (data.session).substring(16))) {
-		return sendToClient('creation-complete', "Session has expired.", '15.' + __line);
+		return callback({"error": "Session has expired.", "id": 1, "line": __line});
 	} else if(data.type < 0 || data.type > 2) {
-		return sendToClient('creation-complete', "Invalid server type.", '16.' + __line);
+		return callback({"error": "Invalid server type.", "id": 2, "line": __line});
 	} else if(typeof data.id == 'number') {
 		
 		// User id specified, get user session
 		user.get(data.id, function(err, line, dat) {
 			if(err) {
-				return sendToClient('creation-complete', err, '17.' + __line + '.' + line);
+				return callback({"error": err, "id": 3, "line": __line});
 			}
 			
 			// Check if session is valid
@@ -22,18 +24,18 @@ exports.create = function createServer(data, IP, callback) {
 				// Session valid, create server
 				mkdir("users/" + data.id + "/server", function(err) {
 					if(err) {
-						return sendToClient('creation-complete', err, '18.' + __line);
+						return callback({"error": err, "id": 4, "line": __line});
 					}
 					
 					fs.writeFile("users/" + data.id + "/server/.properties", "0\n" + data.type + "\n0\n0", function(err, dat) {
 						if(err) {
-							return sendToClient('creation-complete', err, '19.' + __line);
+							return callback({"error": err, "id": 5, "line": __line});
 						}
 						
 						if(data.type == 0) {
 							mcLib.addJar("servers/" + data.id, function(err) {
 								if(err) {
-									return sendToClient('creation-complete', err, '20.' + __line);
+									return callback({"error": err, "id": 6, "line": __line});
 								}
 								
 								sendToClient('creation-complete');
@@ -42,7 +44,7 @@ exports.create = function createServer(data, IP, callback) {
 					});
 				});
 			} else {
-				sendToClient('creation-complete', "Invalid session.", '21.' + __line);
+				return callback({"error": "Invalid session.", "id": 7, "line": __line});
 			}
 		});
 	} else {
